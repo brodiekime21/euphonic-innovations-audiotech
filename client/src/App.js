994e057a0beea00316c79638a7c01074f4abce80
Brodie Kime <brodie.kime@gmail.com>
2023-06-12 17:30:19 -0400

    app.js created and .env

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
+
 import './App.css';
 
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Account from './pages/Account';
+import Signup from './pages/Signup';
+import EditAccount from './pages/EditAccount';
+
 function App() {
+
+  const getToken = () => {
+    return localStorage.getItem("authToken")
+  }
+
+  const LoggedIn = () => {
+    return getToken() ? <Outlet /> : <Navigate to="/" />;
+  };
+
+  const NotLoggedIn = () => {
+    return !getToken() ? <Outlet /> : <Navigate to="/" />;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Navbar />
+
+<Routes>
+
+  <Route path='/' element={<Home />} />
+
+  <Route element={<LoggedIn />}>
+
+  <Route path='/account/:id' element={<Account />} />
+  <Route path='/edit-account/:id' element={<EditAccount />} />
+
+  </Route>
+
+  <Route element={<NotLoggedIn />}>
+
+    <Route path='/signup' element={<Signup />} />
+    <Route path='/login' element={<Login />} />
+
+  </Route>
+
+</Routes>
+
     </div>
   );
 }
