52e8bdd0d33638125372a30b119ccca1b2de5f65
Brodie Kime <brodie.kime@gmail.com>
2023-06-17 05:49:57 -0400

    working on www and created user model

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+    type: String,
+    required: true,
+    unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now
+    },
+  },
+  {
+    timeseries: true,
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
