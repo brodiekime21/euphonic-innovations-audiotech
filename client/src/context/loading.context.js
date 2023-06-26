b02f4cc87d53f708d7d3af25774b1988d5d45f18
Brodie Kime <brodie.kime@gmail.com>
2023-06-25 22:02:33 -0400

    auth and loading services

--- client/src/context/loading.context.js
+++ client/src/context/loading.context.js
@@ -1,34 +1,33 @@
-// import { useState, createContext } from "react";
-// import { get, post } from "../services/authService";
-// import axios from "axios";
+import { useState, createContext } from "react";
+import { get, post } from "../services/authService";
+import axios from "axios";
 
-// const LoadingContext = createContext();
+const LoadingContext = createContext();
 
-// const LoadingProvider = ({ children }) => {
+const LoadingProvider = ({ children }) => {
 
-//     const [isLoading, setIsLoading] = useState(false);
-//     const [user, setUser] = useState(null);
-//     const [message, setMessage] = useState('');
-//     const [sample, setSample] = useState([]);
-//     const [pack, setPack] = useState([])
-//     const [allUsers, setAllUsers] = useState([])
-
-
-//     const setTimedMessage = (newMessage) => {
-//       setMessage(newMessage);
-//       setTimeout(() => {
-//         setMessage('')
-//       }, 1000)
-//     }
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const [message, setMessage] = useState('');
+    const [product, setProduct] = useState([]);
+    const [allUsers, setAllUsers] = useState([])
 
 
-//       return (
-//         <LoadingContext.Provider value={{ allUsers, setAllUsers, pack, setPack, sample, setSample, setUser, user, setIsLoading, setMessage, setTimedMessage }}>
-//           {children}
-//         </LoadingContext.Provider>
-//       );
+    const setTimedMessage = (newMessage) => {
+      setMessage(newMessage);
+      setTimeout(() => {
+        setMessage('')
+      }, 1000)
+    }
 
 
-// }
+      return (
+        <LoadingContext.Provider value={{ allUsers, setAllUsers, product, setProduct, setUser, user, setIsLoading, setMessage, setTimedMessage }}>
+          {children}
+        </LoadingContext.Provider>
+      );
 
-// export { LoadingContext, LoadingProvider }
+
+}
+
+export { LoadingContext, LoadingProvider }
